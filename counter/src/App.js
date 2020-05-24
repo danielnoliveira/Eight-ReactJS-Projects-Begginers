9c3753ee11a0acb1e4f7fb27e49df0cdc0acab58
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-05-24 19:19:11 -0300

    Projeto counter finalizado

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [counter,setCounter] = useState(0);
+
+  const plus = ()=>{
+    setCounter(counter+1);
+  }
+  const minus = ()=>{
+    setCounter(counter-1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App__title">
+        counter
+      </h1>
+      <div className="App__counter">
+        <div className="counter__display">
+          <p className="display__value">
+            {counter}
+          </p>
+        </div>
+        <div className="counter__buttons">
+          <button onClick={minus} className="buttons__minus">
+            -
+          </button>
+          <button onClick={plus} className="buttons__plus">
+            +
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
