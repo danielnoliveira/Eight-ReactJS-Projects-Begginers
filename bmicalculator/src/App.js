a69dae62409d6de86d15167e6b5d4b8984d28778
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-05-26 00:23:11 -0300

    Atualização de layout, exibição da categoria de acordo com o bmi

--- bmicalculator/src/App.js
+++ bmicalculator/src/App.js
@@ -2,10 +2,25 @@ import React,{useState} from 'react';
 
 import './App.css';
 
+function getConclusionBMI(bmi){
+  if(bmi===0){
+    return '';
+  }
+  if(bmi<18.5){
+    return 'Underweight';
+  }
+  if(18.5<=bmi<=24.99){
+    return 'Normal weight';
+  }
+  if(bmi>=30){
+    return 'Obesity';
+  }
+  return 'Overweight';
+}
 function App() {
   const [height,setHeight] = useState(0);
   const [weight,setWeight] = useState(0);
-  const [result,setResult] = useState('');
+  const [result,setResult] = useState(0);
   return (
     <div className="App">
       <h1 className="App__title">bmi calculator</h1>
@@ -29,7 +44,7 @@ function App() {
           setResult((weight/(height**2)).toFixed(2))}}>calculate</button>
       </form>
       <div className="App__result">
-        BMI: {result}
+        BMI: {result} ; Conclusion: {getConclusionBMI(result)}
       </div>
     </div>
   );
