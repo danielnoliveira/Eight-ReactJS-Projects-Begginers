78fbbceb678806ae67fe8ca986cc8b5760db989b
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-05-26 01:51:50 -0300

    Projeto random joke generator terminado

--- randomjokegenerator/src/App.js
+++ randomjokegenerator/src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-
+import React,{useState,useEffect} from 'react';
 import './App.css';
+const api = 'https://official-joke-api.appspot.com/jokes/random';
 
 function App() {
+  const [joke,setJoke] = useState({setup:'',punchline:''});
+  const [loading, setLoading] = useState(false);
+  const getJoke =async () =>{
+    if(loading)
+      return;
+    setLoading(true);
+    const response = await (await fetch(api)).json();
+    setJoke({
+      setup:response.setup,
+      punchline:response.punchline
+    });
+    setLoading(false);
+  }
+  useEffect(()=>{
+   getJoke();
+  },[]);
   return (
     <div className="App">
       <header className="App__header">
         <h1 className="App__title">random joke generator</h1>
       </header>
-      
+      <main className="App__board">
+        <div className="board__joke">
+          Q:{joke.setup}<br/>
+          A:{joke.punchline}
+        </div>
+        <button className="board__button" onClick={getJoke}>
+          Generate
+        </button>
+      </main>
     </div>
   );
 }
