8be477a3f5d230914351b90246a639082af720f6
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-05-26 00:49:03 -0300

    Projeto random joke generator iniciado

---
+++ randomjokegenerator/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+        <h1 className="App__title">random joke generator</h1>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
