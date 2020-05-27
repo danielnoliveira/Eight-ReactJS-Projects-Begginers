98c5443e07fbece85f0177a0ad393211732cc58a
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-05-27 19:12:31 -0300

    Iniciando projeto form validation

---
+++ formvalidation/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+        <h1 className="App__title">form validation</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
